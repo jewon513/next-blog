8f47aa5b7ae36f7293696d466576d4777417d238
박제원 <jewon513@gmail.com>
2021-12-29 16:22:57 +0900

    태그 필터 기능 추가

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,11 +1,7 @@
-import Layout from "../../components/Layout";
-import {GetStaticProps} from "next";
-
 const AboutPage = ()=>{
   return (
-    <Layout>
-
-    </Layout>
+    <>
+    </>
   )}
 
 export default AboutPage
